6dec39df3e9e692dd6dda77fa6765391878954ef
EC2 Default User <ec2-user@ip-172-31-104-119.ec2.internal>
2024-10-06 12:12:46 +0000

    1 validator latency results added

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         "cryptography==42.0.8",
         "pandas==2.2.3",
         "matplotlib==3.9.2",
-        "httpx==0.27.2"
+        "httpx==0.27.2",
+        "typer==0.12.3"
     ],
     classifiers=[
         # See https://pypi.org/classifiers/ for a list of valid classifiers
